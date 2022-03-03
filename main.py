fc7a70331d770b91bdc46cf4156b53ccfbd522ab
SuperDumbTM <eltonlochunkit@gmail.com>
2022-03-03 21:08:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ ROTATE_FLAG = False
 month = ["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"]
 # settinh
 today = (date.today().strftime("%d"),date.today().strftime("%m"),date.today().strftime("%Y"),
-    date.today().strftime("%a"),date.today().strftime("%b"))
-str_date = today[0] + " " + today[4].upper() + ", " + today[2] + " " + today[3].upper()
+    date.today().strftime("%A"),date.today().strftime("%b"))
+str_date = today[0] + " " + ", " + today[2] + " | " + today[3].upper()
 L_icon_size = 80, 80
 S_icon_size = 60, 60
 # set font

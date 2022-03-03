f55e5776fb2f1f1bc084b8cb30ed4c39d958b63c
SuperDumbTM <eltonlochunkit@gmail.com>
2022-03-03 12:33:49 +0800

    wx testing

--- lib/weather_info.py
+++ lib/weather_info.py
@@ -1,27 +1,30 @@
+import json, requests
+
+
 class WeatherInfo:
     
     def __init__(self) -> None:
         self.dist = "香港天文台"
-        self.rain_dist = "油尖旺"
+        self.rainfall_dist = "油尖旺"
 
     @classmethod
     # with specified location
-    def WeatherInfo(self,dist,rain_dist):
+    def WeatherInfo(self,dist,rainfall_dist):
         self.dist = dist
-        self.rain_dist = rain_dist
+        self.rain_dist = rainfall_dist
     
-    def rhrread_process(): # 本港地區天氣報告
+    def rhrread_process(self): # 本港地區天氣報告
         rhrread_url = 'https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=rhrread&lang=tc'
         rhrread_data = json.loads(requests.get(rhrread_url).text)
 
         for n_0 in range(len(rhrread_data["temperature"]["data"])):
-            if rhrread_data["temperature"]["data"][n_0]["place"] ==dist:
+            if rhrread_data["temperature"]["data"][n_0]["place"] ==self.dist:
                 break
         if n_0 >= len(rhrread_data["temperature"]["data"])-1:
             n_0 = 1
 
         for n_1 in range(len(rhrread_data["rainfall"]["data"])):
-            if rhrread_data["rainfall"]["data"][n_1]["place"] ==rainfall_dist:
+            if rhrread_data["rainfall"]["data"][n_1]["place"] ==self.rainfall_dist:
                 break
         if n_1 >= len(rhrread_data["rainfall"]["data"]):
             n_1 = 1
@@ -30,7 +33,7 @@ class WeatherInfo:
         print('地區 :', rhrread_data["temperature"]["data"][n_0]["place"])
         print('氣溫 :', rhrread_data["temperature"]["data"][n_0]["value"],'°C')
         print('濕度 :', rhrread_data["humidity"]["data"][0]["value"],'%')
-        # print('濕度資料位置 :', rhrread_data["humidity"]["data"][0]["place"])
+        print('濕度資料位置 :', rhrread_data["humidity"]["data"][0]["place"])
         print('圖示 :',  + rhrread_data["icon"][0])
 
         print('雨量資料位置 :', rhrread_data["rainfall"]["data"][n_1]["place"])
---
+++ wx_test.py
@@ -0,0 +1,5 @@
+from lib import weather_info
+
+wx = weather_info.WeatherInfo()
+
+wx.rhrread_process()
